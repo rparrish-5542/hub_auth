aefc6281436d2a697314fb3bb20737256df81661
rparrish-5542 <rparrish@wedgwood.org>
2025-12-10 11:45:04 -0500

    v1.0.38: Clean up migration conflicts - remove duplicate migrations and add merge migration

--- hub_auth_client/django/migrations/0005_remove_allowed_audiences.py
+++ hub_auth_client/django/migrations/0005_remove_allowed_audiences.py
@@ -11,7 +11,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('hub_auth_client', '0004_apiendpointmapping'),
+        ('hub_auth_client', '0004_rename_hub_auth_cl_url_pat_idx_hub_auth_cl_url_pat_6a85ac_idx_and_more'),
     ]
 
     operations = [
